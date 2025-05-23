e7c9221df1def71710155510067358e175cfb2aa
lvdEphec <33726310+lvdEphec@users.noreply.github.com>
2025-05-23 14:03:26 +0200

    Add files via upload

---
+++ produits-q2.js
@@ -0,0 +1,26 @@
+/* Fichiers sources:
+
+https://github.com/lvdEphec/tp-progra
+
+Q2--- Structures et données
+
+Objectifs principaux :
+* Afficher les produits de la DB dans la page
+* Ajouter un produit 
+
+0) Créer la base de données des acteurs, ou la télécharger
+Pas à faire à l'examen, mais intéressant à bien comprendre.
+
+1) Créer un webservice pour récupérer 
+1.1) Créer une procédure avce le SELECT
+1.2) Créer le webservice
+
+2) Faire un appel pour récupérer les données en json et les afficher
+
+3) Construire une table sur base des données reçues
+
+4) Ajouter un acteur via le formulaire.
+4.1) Créer le webservice
+4.2) Ajouter l'acteur depuis la page web et le formulaire.
+
+*/
