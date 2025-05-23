b3a79799c7b4794c7368e460cb9da8ef43df3c06
lvdEphec <33726310+lvdEphec@users.noreply.github.com>
2025-05-23 16:06:06 +0200

    Add files via upload

---
+++ produits-q1.js
@@ -0,0 +1,25 @@
+/* Fichiers sources:
+
+https://github.com/lvdEphec/tp-progra
+
+Q1--- Logique et programmations
+
+Se baser en local sur ces données d'acteurs défini dans produits-q1.js
+
+Construire une table sur base de ces données.
+
+Créer un index qui trie sur le Et construire la table sur cet index
+
+Ajouter un produit en utilisant le formulaire. [Note: pour la catégorie on hardcode toujours la même ici]
+
+Créer une fonction qui calcule le prix moyen. Puis une fonction qui affiche en console le prix moyen en utilisant la fonction précédente.
+
+*/
+
+let index = ['p02','p03'];
+let courses = {
+    p01:{nom: table,     montant: 50},    
+    p02:{nom: ordinateur,     montant: 1000},    
+    p03:{nom: consolle,     montant: 600}
+	}
+};
